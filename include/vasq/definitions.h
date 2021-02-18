03bcc9d38694b63644053974ec894d5253aababb
nickeldan <nickeldan@gmail.com>
2021-02-17 22:23:01 -0500

    added changelog; removed definitions of VASQ_RET_REDIRECT_FAIL and VASQ_RET_FORK; added the VASQ_VERSION macro definition; the clean target of the Makefile now removes logger_test; changed the version to 3.0.0

--- include/vasq/definitions.h
+++ include/vasq/definitions.h
@@ -18,11 +18,11 @@
 enum vasqRetValue {
     VASQ_RET_OK = 0,
     VASQ_RET_USAGE,
-    VASQ_RET_REDIRECT_FAIL,
     VASQ_RET_OUT_OF_MEMORY,
-    VASQ_RET_FORK,
 
     VASQ_RET_UNUSED,
 };
+
+#define VASQ_VERSION "3.0.0"
 
 #endif  // VANILLA_SQUAD_DEFINITIONS_H
